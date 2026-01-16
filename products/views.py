21eca29812b34162b41d6addc0eb2ce1d9326b67
BrenoMatheus <breno.moj99@gmail.com>
2026-01-16 14:14:57 -0300

    feat: criar tela de consulta de produtos com busca por nome

--- products/views.py
+++ products/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import Product
+
+
+def product_list(request):
+    search = request.GET.get("q")
+
+    if search:
+        products = Product.objects.filter(name__icontains=search)
+    else:
+        products = Product.objects.all()
+
+    return render(request, "products/product_list.html", {
+        "products": products,
+        "search": search or ""
+    })
