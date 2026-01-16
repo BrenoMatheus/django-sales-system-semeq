af03eaf9d4f89f0dc63f458e6e265175a02ea829
BrenoMatheus <breno.moj99@gmail.com>
2026-01-16 12:31:35 -0300

    feat: registrar apps e gerar migrations iniciais

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Apps do projeto
+    'products.apps.ProductsConfig',
+    'customers.apps.CustomersConfig',
+    'sales.apps.SalesConfig',
+    'users.apps.UsersConfig',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
