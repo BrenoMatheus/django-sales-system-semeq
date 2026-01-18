4ba89270914d262a14e8d32823bbb601185aad46
BrenoMatheus <breno.moj99@gmail.com>
2026-01-18 10:53:41 -0300

    refactor: ajusta estrutura do formulário de checkout e prepara fluxo de sucesso

--- sales/urls.py
+++ sales/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('nova/', views.create_sale, name='create_sale'),
     path("checkout/", views.checkout, name="checkout"),
+    path("sucesso/", views.order_success, name="order_success")
+
 ]
--- sales/views.py
+++ sales/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def create_sale(request):
     return render(request, 'sales/create_sale.html')
 
 def checkout(request):
+    if request.method == "POST":
+        # 1. Captura dados do endereço
+        # 2. Cria Sale
+        # 3. Cria SaleItem
+        # 4. Limpa carrinho
+
+        return redirect("order_success")
+
     return render(request, "checkout.html")
+
+def order_success(request):
+    return render(request, "order_success.html")
+
+def order_success(request):
+    return render(request, "order_success.html")
